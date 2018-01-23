977268a6d6d7814504023b5b0c1511551aa96311
Samuel Kim <samuel79.kim@samsung.com>
2018-01-23 16:05:44 +0900

    storyboard 작업

---
+++ FinancialFreeApps/ChartTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ChartTableViewCell.swift
+//  FinancialFreeApps
+//
+//  Created by Samuel Kim on 2018. 1. 23..
+//  Copyright © 2018년 Samuel Kim. All rights reserved.
+//
+
+import UIKit
+
+class ChartTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var appIcon: UIImageView!
+    @IBOutlet weak var appName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
